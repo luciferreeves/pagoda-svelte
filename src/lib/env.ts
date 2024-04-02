0d9a512c077cac5ac7f305c54623193e16882ac8
Bobby <bobbyskhs@gmail.com>
2024-04-02 11:56:36 -0400

    Basic User auth

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -6,8 +6,8 @@ const env_dev = {
 };
 
 const env_prod = {
-    api: '',
-    ws: '',
+    api: 'https://shi.foo',
+    ws: 'wss://shi.foo',
 };
 
 export const env = dev ? env_dev : env_prod;
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,5 +3,6 @@ export const ssr = false;
 import { getCurrentUser } from "$lib/api/currentUser";
 import { currentLoggedInUser } from "../store";
 
-const currentUser = await getCurrentUser();
-currentLoggedInUser.set(currentUser);
+getCurrentUser().then(currentUser =>
+    currentLoggedInUser.set(currentUser)
+);
